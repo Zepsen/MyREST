8b96bff555bb0e183399f2d7fcd44e68d7a2df1b
Zepsen <zepsendm@gmail.com>
2018-11-06 15:03:50 +0200

    гз

---
+++ Server/DAL/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Microsoft.AspNetCore.Identity;
+
+namespace DAL.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public User User { get; set; }
+    }
+}
---
+++ Server/WEB/Infrastructure/Hubs/ApplicationHub.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using WEB.Models.Hubs;
+
+namespace WEB.Infrastructure.Hubs
+{
+    public class ApplicationHub : Hub
+    {
+        public async Task Notification(NotificationHubModel model)
+        {
+            await Clients.All.SendAsync("notificationReceived", "server", model.Message);
+        }
+    }
+}
